d7344fd24d6937bb1dc010e719567ecf06a486ac
ZeynepTikir <zeyneptikir@marun.edu.tr>
2025-01-01 20:35:04 +0300

    Update bir_araliktaki_asallari_yazdirma.cpp

--- bir_araliktaki_asallari_yazdirma.cpp
+++ bir_araliktaki_asallari_yazdirma.cpp
@@ -1,44 +1,50 @@
-//belirlenen iki sayý arasýndaki asallarý ve ortalamasýný ekrana yazdýrýr.
+//belirlenen iki sayi arasindaki asallari ve ortalamasini ekrana yazdirir.
 #include <stdio.h>
-#include <conio.h>
+
 main(){
-	int a=0,b=0,buyuk=0,kucuk=0,asaldegil=0;
+	//Degiskenler tanimlandi.
+	int a=0,b=0;
+	int buyuk=0,kucuk=0;
+	int asaldegil=0;
 	float topla=0,adet=0;
+
+	//Kullanicidan 2 sayi alinir
 	printf ("1. sayiyi giriniz: ");
 	scanf("%d",&a);
 	printf("2. sayiyi giriniz: ");
 	scanf ("%d",&b);
 
-    if (a>b){
-       buyuk=a;
-       kucuk=b;
-    }
-    else{
-       buyuk=b;
-       kucuk=a;
-    }
-       
-    for(int sayi=kucuk;sayi<buyuk;sayi++){               
-		for(int i=2;i<sayi;i++){
-			if(sayi%i==0)
-            	asaldegil++;
+	//Kullanicinin girdigi hangi sayinin buyuk hangisinin kucuk oldugu belirlenir
+   	if (a>b){
+       		buyuk=a;
+       		kucuk=b;
     	}
-		
-		if (asaldegil==0&& sayi!=1){
-			printf ("%d, ",sayi);
+    	else{
+       		buyuk=b;
+       		kucuk=a;
+    	}
+       
+    	for(int sayi=kucuk;sayi<buyuk;sayi++){    
+		//kucuk sayidan baslayarak asallik kontrolu yapÄ±lacak.
+		for(int i=2;i<sayi;i++){
+			//kontrol edilen sayiyi bolebilen sayi var mi diye bakilir
+			if(sayi%i==0)
+            			asaldegil++;
+    		}
+		//eger asaldegil degiskeninin degeri 0 kalmissa asal oldugu anlasilir.
+		if (asaldegil==0 && sayi!=1){
+			//araliktaki asal sayilar alt alta yazdirilacak
+			printf ("%d\n",sayi);
+			//sayilarin adetleri ve toplamlari bulunacak.
 			topla+=sayi;
 			adet++;
-			/*if()
-			printf(", ");
-			else
-			printf(". ");*/
-		
 		}
-		
+		//sonraki sayinin kontrolu icin kontrol degiskeninin degeri sifirlanir.
    		asaldegil=0;
-    }
-    
-    printf("\n\nOrtalama= %.2f",topla/adet);
-       
-       getch();
+    	}
+
+	//ortalama ekrana yazdirilir.
+    	printf("\n\nOrtalama= %.2f",topla/adet);
+
+	return 0;
 }
