3153b032a059ad7c45c5f95a1db15eef7c48f468
ZeynepTikir <zeyneptikir@marun.edu.tr>
2025-01-01 20:22:17 +0300

    Comment 5_str_scanf.cpp

--- 5_str_scanf.cpp
+++ 5_str_scanf.cpp
@@ -1,28 +1,33 @@
 #include <stdio.h>
-#include <conio.h>
 #include <locale.h>
 
 main() {
 	setlocale(LC_ALL, "Turkish");
+	
 	double x;
 	int n;
 	char str1[20], str2[30], ad[20];
 	
 	printf("Ad-Soyad: ");
+
+	//Whitespace iceren metin alma:
 	gets(ad);
-	printf("Adýnýz: %s dir.\n \n", ad);
 	
-	printf("Lütfen giriniz bir integer: ");
+	printf("AdÄ±nÄ±z: %s dir.\n \n", ad);
+	
+	printf("LÃ¼tfen giriniz bir integer: ");
 	scanf("%d", &n);
-	printf("Ýnteger karekterimiz %d dir.\n\n", n);
-	printf("Lütfen giriniz bir double: ");
+	printf("Ä°nteger karekterimiz %d dir.\n\n", n);
+	
+	printf("LÃ¼tfen giriniz bir double: ");
 	scanf("%lf", &x);
 	printf("Double karekterimiz %g idi.\n\n", x);
-	printf("Lütfen giriniz bir integer ve bir floating-point sayý: ");
-	scanf("%d%lf", &n, &x);
-	printf("Sayýlarýmýz %d ve %g idi.\n", n, x);
 	
-	printf("Ýsim gir: ");
+	printf("LÃ¼tfen giriniz bir integer ve bir floating-point sayÄ±: ");
+	scanf("%d %lf", &n, &x);
+	printf("SayÄ±larÄ±mÄ±z %d ve %g idi.\n", n, x);
+	
+	printf("Ä°sim gir: ");
 	scanf("%s", &str1);
 	
 	printf("Web adresini gir: ");
@@ -31,6 +36,5 @@ main() {
 	printf("Girilen isim: %s\n", str1);
 	printf("Girilen Web site: %s\n ", str2);
 	
-	getch ();
-	
+	return 0;
 }
